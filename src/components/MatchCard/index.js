04de9de135120b577db9e8870c1d4c2cbd38aeb0
kranthikasarla <kranthitiru094@gmail.com>
2023-07-26 14:37:09 +0000

    IPL dashboard

---
+++ src/components/MatchCard/index.js
@@ -0,0 +1,23 @@
+// Write your code here
+import {Component} from 'react'
+import './index.css'
+
+class MatchCard extends Component {
+  render() {
+    const {matchesData} = this.props
+    const {result, competingTeam, competingTeamLogo, matchStatus} = matchesData
+    return (
+      <li className={`match-card ${matchStatus}`}>
+        <img
+          className="match-card-logo"
+          src={competingTeamLogo}
+          alt={`competing team ${competingTeam}`}
+        />
+        <p className="match-card-name">{competingTeam}</p>
+        <p className="match-card-result">{result}</p>
+        <p className="match-status">{matchStatus}</p>
+      </li>
+    )
+  }
+}
+export default MatchCard
